93b060b24d628e0d6357ce8391c2ba037227eb74
trentycha <trentycharlene@gmail.com>
2026-02-17 14:52:32 +0100

    Section with buttons in homepage added

--- frontend/src/components/HomeCard.jsx
+++ frontend/src/components/HomeCard.jsx
@@ -5,7 +5,7 @@ const HomeCard = (props) => {
 
 
   return (
-    <div onClick={() => navigate(`/${props.id}`)} className="bg-[#343434] rounded-2xl shadow-xl shadow-black my-6 px-10 py-8 flex-1">
+    <div onClick={() => navigate(`/${props.id}`)} className="bg-[#343434] rounded-2xl shadow-xl shadow-black/60 my-6 px-10 py-8 flex-1">
       <p className="text-white pb-5">{props.status}</p>
       <h3 className="text-[#F9FF00] font-pixeloid-bold text-lg">{props.name}</h3>
       <p className="text-[#00DEF5]">Room - {props.channel}</p>
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -32,15 +32,40 @@ const Homepage = () => {
         </div>
         
         <div className="mt-20 text-xl">
-          <h4 className='text-[#00DEF5]'>Prochain tournois</h4>
+          <h4 className='text-[#00DEF5] font-bold'>Prochains tournois...</h4>
         </div>
-
         <div className="flex gap-10">
           {cards.map(c => (<HomeCard key={c.id} id={c.id} status={c.tournamentStatus.label} name={c.name} channel={c.channel.label}/>))}
         </div>
 
         <div>
           <img src='/images/separation.png' alt="pacman-ghost-separation" className="w-100 mx-100 my-20"/>
+        </div>
+
+        <div className="flex items-center gap-6">
+          <div className="">
+            <a href='/' className="relative">
+              <img src='/images/tournaments.jpg' alt="tounrament-home" className="w-170 rounded-xl opacity-50 shadow-xl shadow-black"/>
+              <h3 className="absolute top-12 left-12 text-[#F9FF00] font-pixeloid-bold text-3xl">Tous les tournois</h3>
+              <span className="absolute top-87 left-115 bg-[#00DEF5] px-7 py-4 rounded-lg text-white text-xl font-semibold shadow-lg shadow-black/30">Je découvre !</span>
+            </a>
+          </div>
+          <div className=" relative">
+            <a href='/' className="my-10 mx-10">
+              <img src='/images/login-home.jpg' alt="tournament-home" className="w-125 rounded-xl opacity-50 shadow-xl shadow-black"/>
+              <h3 className="absolute top-15 left-10 text-[#F9FF00] font-pixeloid-bold text-3xl">A vous de jouer !</h3>
+              <span className="absolute top-35 left-65 bg-[#00DEF5] px-7 py-4 rounded-lg text-white text-xl font-semibold shadow-lg shadow-black/30">Je me connecte !</span>
+            </a>
+            <a href='/' className="my-10 mx-10">
+              <img src='/images/about.jpg' alt="tounrament-home" className="w-125 rounded-xl opacity-50 shadow-xl shadow-black"/>
+              <h3 className="absolute top-72 left-10 text-[#F9FF00] font-pixeloid-bold text-3xl">A propos de <br/> RetroChampionship</h3>
+              <span className="absolute top-98 left-72 bg-[#00DEF5] px-7 py-4 rounded-lg text-white text-xl font-semibold shadow-lg shadow-black/30">Qui sont-ils ?</span>
+            </a>
+          </div>
+        </div>
+
+        <div className="mt-20 text-xl">
+          <h4 className='text-[#00DEF5] font-bold'>Les sponsors de RetroChampionship</h4>
         </div>
 
       </div>
