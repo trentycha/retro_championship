7818652438579bb4350c9646dc9752857dee7fc7
trentycha <trentycharlene@gmail.com>
2026-02-19 09:36:41 +0100

    title from profile page updated for logout user

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -29,7 +29,9 @@ const Profile = () => {
     <div className="bg-[#272727]">
         <div className="px-40 pb-20">
 
-            <h2 className="text-[#F9FF00] font-pixeloid-bold text-4xl">Salut, {user.username}</h2>
+            {isOwner ?
+                <h2 className="text-[#F9FF00] font-pixeloid-bold text-4xl">Salut, {user.username}</h2>
+            : <h2 className="text-[#F9FF00] font-pixeloid-bold text-4xl">{user.username}</h2>}
 
             <div className="flex gap-6">
                 <div className="bg-[#343434] rounded-2xl shadow-xl shadow-black/30 flex items-start py-10 px-10 mt-10">
