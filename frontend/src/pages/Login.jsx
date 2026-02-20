23cb089cdd4c79dcf2e56a1f95cf993f1a919b72
trentycha <trentycharlene@gmail.com>
2026-02-20 14:32:58 +0100

    responsive: login page updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useAuth } from '../hooks/useAuth.js'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom'
+import Loading from './Loading.jsx'
 
 const Login = () => {
   const { login } = useAuth();
@@ -26,31 +26,31 @@ const Login = () => {
       }
 
       if(loading) {
-        <div>Chargement...</div>
+        return <Loading />;
       }
   }
 
   return (
     <div className="bg-[#272727]">
-      <div className="px-60">
+      <div className="px-6 py-6 xl:px-60">
         <div className="bg-[#343434] rounded-2xl shadow-xl shadow-black/30 flex items-start">
 
-            <div className="flex-1">
+            <div className="hidden xl:block xl:flex-1">
                 <img src='/images/login.jpg' alt="login" className="w-full rounded-l-xl"/>
             </div>
 
-            <form onSubmit={handleSubmit} className="flex-1 pl-15">
-                <h2 className="text-[#F9FF00] font-pixeloid-bold text-3xl mt-15 mb-10">Connectez-vous !</h2>
-                <p className='text-[#00DEF5] ml-3'>Email</p>
-                <input placeholder="Votre email" className='bg-[#4F4F4F] px-4 py-3 pr-60 rounded-lg mb-4 mt-1'
+            <form onSubmit={handleSubmit} className="px-10 py-12 xl:flex-1 xl:pl-15 xl:px-0 xl:py-10">
+                <h2 className="text-[#F9FF00] font-pixeloid-bold text-lg xl:text-3xl xl:mt-5 xl:mb-10">Connectez-vous !</h2>
+                <p className='text-[#00DEF5] pt-4 xl:pt-0 xl:ml-3'>Email</p>
+                <input placeholder="Votre email" className='bg-[#4F4F4F] text-white px-4 py-3 pr-12 xl:pr-60 rounded-lg mb-4 mt-1'
                 value = {mail}
                 onChange={(e) => setMail(e.target.value)}></input>
-                <p className='text-[#00DEF5] ml-3'>Mot de passe</p>
-                <input placeholder="Votre mot de passe" className='bg-[#4F4F4F] px-4 py-3 pr-60 rounded-lg mb-4 mt-1'
+                <p className='text-[#00DEF5] xl:ml-3'>Mot de passe</p>
+                <input type="password" placeholder="Votre mot de passe" className='bg-[#4F4F4F] text-white px-4 py-3 pr-12 xl:pr-60 rounded-lg mb-4 mt-1'
                 value = {password}
                 onChange={(e) => setPassword(e.target.value)}></input>
-                <button type="submit" className="bg-[#00DEF5] hover:bg-white hover:text-[#00DEF5] px-7 py-4 rounded-lg text-white text-xl font-semibold shadow-lg shadow-black/30 mt-6">Je me connecte !</button>
-                {error && <p className="text-red-500">{error}</p>}
+                {error && <p className="text-red-500">Mail ou mot de passe incorrect</p>}
+                <button type="submit" className="bg-[#00DEF5] hover:bg-white hover:text-[#00DEF5] px-7 py-4 rounded-lg text-white xl:text-xl font-semibold shadow-lg shadow-black/30 mt-6">Je me connecte !</button>
             </form>
 
         </div>
